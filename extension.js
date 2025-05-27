8f856ea35145188b9ed2ef856c612f845075fa08
haminic <hamkittichet@gmail.com>
2025-05-27 16:55:42 +0700

    Remove smart tabbing, fix bug with pressing enter before the prefix

--- extension.js
+++ extension.js
@@ -7,9 +7,6 @@ function activate(context) {
 	context.subscriptions.push(
 		vscode.commands.registerCommand("list-continuation.onEnter", onEnter)
 	);
-	context.subscriptions.push(
-		vscode.commands.registerCommand("list-continuation.onTab", onTab)
-	);
 }
 
 function deactivate() {}
@@ -25,76 +22,59 @@ function onEnter() {
 
 	const config = vscode.workspace.getConfiguration("list-continuation");
 	const rules = config.get("rules") || {};
-	const langRules = rules[editor.document.languageId];
+	const langID = editor.document.languageId;
+	const langRules = rules[langID];
+	
+	if (!Array.isArray(langRules) || langRules.length === 0 || config.get("disabled-languages").includes(langID)) {
+		return vscode.commands.executeCommand("type", { source: "keyboard", text: "\n" });
+	}
 
-	if (!Array.isArray(langRules) || langRules.length === 0) {
+	let matchedNothing = true;
+	const actions = [];
+	
+	for (const selection of editor.selections) {
+		const cursorPos = selection.active;
+		const line = editor.document.lineAt(cursorPos.line);
+		const indentation = line.text.substring(0, line.firstNonWhitespaceCharacterIndex);
+
+		let matched = false;
+		for (const rule of langRules) {
+			const regexString = "^\\s*" + escapeForRegex(rule.start);
+
+			// removes line if the prefix is there, but line is empty
+			if (rule.removeIfEmpty && (new RegExp(`${regexString}\\s*$`)).test(line.text)) {
+				const rangeToDelete = line.range.with(cursorPos.with(line.lineNumber, line.firstNonWhitespaceCharacterIndex), line.range.end);
+				actions.push(editBuilder => editBuilder.delete(rangeToDelete));
+				matched = true;
+				matchedNothing = false;
+				break;
+			} 
+
+			// continues the list if prefix is there, and cursor is after the prefix 
+			const match = (new RegExp(`(${regexString})\\s*[^\\s]+.*`)).exec(line.text);
+			if (match && cursorPos.character >= match[1].length) {
+				const itemString = `\n${indentation}${rule.continue}`;
+				actions.push(editBuilder => editBuilder.insert(cursorPos, itemString));
+				matched = true;
+				matchedNothing = false;
+				break;
+			} 
+		}
+
+		if (!matched) {
+			actions.push(editBuilder => editBuilder.insert(cursorPos, `\n${indentation}`));
+		}
+	}
+
+	// if matches nothing, use default enter behavior
+	if (matchedNothing) {
 		return vscode.commands.executeCommand("type", { source: "keyboard", text: "\n" });
 	}
 
 	return editor.edit(editBuilder => {
-		for (const selection of editor.selections) {
-			const cursorPos = selection.active;
-			const line = editor.document.lineAt(cursorPos.line);
-			const indentation = line.text.substring(0, line.firstNonWhitespaceCharacterIndex);
-
-			let matched = false;
-			for (const rule of langRules) {
-				const regexString = "^\\s*" + escapeForRegex(rule.start);
-
-				if (rule.removeIfEmpty && (new RegExp(`${regexString}\\s*$`)).test(line.text)) {
-					const rangeToDelete = line.range.with(cursorPos.with(line.lineNumber, line.firstNonWhitespaceCharacterIndex), line.range.end);
-					editBuilder.delete(rangeToDelete);
-					matched = true;
-					break;
-				} else if ((new RegExp(`${regexString}\\s*[^\\s]+.*`)).test(line.text)) {
-					const itemString = `\n${indentation}${rule.continue}`;
-					editBuilder.insert(cursorPos, itemString);
-					matched = true;
-					break;
-				} 
-			}
-
-			if (!matched) {
-				editBuilder.insert(cursorPos, `\n${indentation}`);
-			}
-		}
-	});
-
-}
-
-function onTab() {
-	const editor = vscode.window.activeTextEditor;
-	if (!editor) { return; }
-
-	const config = vscode.workspace.getConfiguration("list-continuation");
-	const rules = config.get("rules") || {};
-	const langRules = rules[editor.document.languageId];
-	
-	if (!Array.isArray(langRules) || langRules.length === 0) {
-		return vscode.commands.executeCommand("tab");
-	}
-
-	return editor.edit(editBuilder => {
-		for (const selection of editor.selections) {
-			const cursorPos = selection.active;
-			const line = editor.document.lineAt(cursorPos.line);
-
-			let matched = false;
-			for (const rule of langRules) {
-				const regexString = "^\\s*" + escapeForRegex(rule.start);
-
-				if ((new RegExp(`${regexString}\\s*$`)).test(line.text)) {
-					const insertPos = cursorPos.with(line.lineNumber, line.firstNonWhitespaceCharacterIndex);
-					editBuilder.insert(insertPos, "\t");
-					matched = true;
-					break;
-				}
-			}
-
-			if (!matched) {
-				editBuilder.insert(cursorPos, "\t");
-			}
-		}
+		actions.forEach(action => {
+			action(editBuilder);
+		});
 	});
 
 }
